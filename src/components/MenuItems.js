b7d49a48d339c90152ac2364ef8d066d944693ac
ibaslogic <ibaskunle@gmail.com>
2022-04-15 17:15:03 +0100

    first update to the last version

---
+++ src/components/MenuItems.js
@@ -0,0 +1,92 @@
+import { useState, useEffect, useRef } from "react";
+
+import Dropdown from "./Dropdown";
+
+const MenuItems = ({ items, depthLevel }) => {
+  const [dropdown, setDropdown] = useState(false);
+
+  let ref = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (dropdown && ref.current && !ref.current.contains(event.target)) {
+        setDropdown(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+    document.addEventListener("touchstart", handler);
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener("mousedown", handler);
+      document.removeEventListener("touchstart", handler);
+    };
+  }, [dropdown]);
+
+  const onMouseEnter = () => {
+    window.innerWidth > 960 && setDropdown(true);
+  };
+
+  const onMouseLeave = () => {
+    window.innerWidth > 960 && setDropdown(false);
+  };
+
+  return (
+    <li
+      className="menu-items"
+      ref={ref}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      {/* if item has url and submenu, we make the button visit the url while still showing dropdown on hover. If no url, we only show hover without linking the button. Else, we render a simple <a> element. Be aware that they are internal links, so we will use the <Link> component from react-router. Here we are using the <a> for simplicity. */}
+      {items.url && items.submenu ? (
+        <>
+          <button
+            type="button"
+            aria-haspopup="menu"
+            aria-expanded={dropdown ? "true" : "false"}
+            // onClick={() => setDropdown((prev) => !prev)}
+          >
+            <a href={items.url}>{items.title}</a>
+            {/* {items.title}{" "} */}
+            {depthLevel > 0 ? (
+              <span>&raquo;</span>
+            ) : (
+              <span
+                className={`arrow${
+                  items.url && items.submenu ? " custom" : ""
+                }`}
+              />
+            )}
+          </button>
+          <Dropdown
+            depthLevel={depthLevel}
+            submenus={items.submenu}
+            dropdown={dropdown}
+          />
+        </>
+      ) : !items.url && items.submenu ? (
+        <>
+          <button
+            type="button"
+            aria-haspopup="menu"
+            aria-expanded={dropdown ? "true" : "false"}
+            onClick={() => setDropdown((prev) => !prev)}
+          >
+            {/* <a href="/#">{items.title}</a> */}
+            {items.title}{" "}
+            {depthLevel > 0 ? <span>&raquo;</span> : <span className="arrow" />}
+          </button>
+          <Dropdown
+            depthLevel={depthLevel}
+            submenus={items.submenu}
+            dropdown={dropdown}
+          />
+        </>
+      ) : (
+        <a href={items.url}>{items.title}</a>
+      )}
+    </li>
+  );
+};
+
+export default MenuItems;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/App";
+// styles
+import "./app.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
